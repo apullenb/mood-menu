4e729c364e94584fc33b3a6eb7b4e079f8bca8bd
Andrea Pullen <apullenb@gmail.com>
2021-10-14 14:35:25 -0500

    header and footer

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css'
+
+
+function Header() {
+    return (
+        <div className='head'>
+          <h1> Mood Menu</h1>
+          <h2>Self Care Activities to Match Your Mood</h2>
+        </div>
+    )
+}
+
+
+
+
+
+export default Header
